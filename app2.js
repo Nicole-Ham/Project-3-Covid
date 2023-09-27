18865339a13c788b35e513bbcf564ada461c21b1
isabelmwork <isabelmwork@gmail.com>
2023-09-27 15:10:56 -0700

    fixed API throttle

--- app2.js
+++ app2.js
@@ -3,27 +3,32 @@
 
 //Case Surveillance API query
 $.ajax({
-    url: "https://data.cdc.gov/resource/n8mc-b4w4.json",
+    url: "https://data.cdc.gov/resource/n8mc-b4w4.json?res_state=CA",
     type: "GET",
     data: {
-      "$limit" : 5000,
+      "$limit" : 10000,
       "$$app_token" : "iwYpUxUglJyjEqOoTODioBzcV"
     }
 }).done(function(data) {
   alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
+  //console.log(data);
+
+  var json = JSON.stringify(data);
+  console.log(json);
+  //var fs = require('fs');
+  //fs.writeFile('myjsonfile.json', json, 'utf8', callback);
 });
 
-//Vaccinations Up-to-Date Status
-$.ajax({
-    url: "https://data.cdc.gov/resource/9b5z-wnve.json",
-    type: "GET",
-    data: {
-      "$limit" : 5000,
-      "$$app_token" : "iwYpUxUglJyjEqOoTODioBzcV"
-    }
-}).done(function(data) {
-  alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
-});
+// //Vaccinations Up-to-Date Status
+// $.ajax({
+//     url: "https://data.cdc.gov/resource/9b5z-wnve.json?Location=CA",
+//     type: "GET",
+//     data: {
+//       "$limit" : 100000000,
+//       "$$app_token" : "iwYpUxUglJyjEqOoTODioBzcV"
+//     }
+// }).done(function(data) {
+//   alert("Retrieved " + data.length + " records from the dataset!");
+//   //console.log(data);
+// });
 
