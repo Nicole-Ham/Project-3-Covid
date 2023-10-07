18c17a34cbce3b131e880e31f011f2afb61ad932
Jeff North <jnorthdata@gmail.com>
2023-10-06 19:26:59 -0700

    friday save

--- Covid_Webpage/scripts.js
+++ Covid_Webpage/scripts.js
@@ -10,36 +10,35 @@ document.addEventListener("DOMContentLoaded", function() {
     }).addTo(map);
 
 
-// Define your bins.
-const bins = [-130000, -4000, -3000, -2500, -500, 0, 500, 1000, 41000];
+    // Define your bins.
+    const bins = [-130000, -4000, -3000, -2500, -500, 0, 500, 1000, 41000];
 
-// Fetch colors corresponding to each bin's position on the 'viridis' scale.
-const colors = bins.map((_, i) => chroma.scale('viridis')(i / (bins.length - 1)).hex());
+    // Fetch colors corresponding to each bin's position on the 'viridis' scale.
+    const colors = bins.map((_, i) => chroma.scale('viridis')(i / (bins.length - 1)).hex());
 
-// Function to get color based on value with explicit binning
-function getColorForValue(value) {
-    for (let i = 0; i < bins.length - 1; i++) {
-        if (value >= bins[i] && value < bins[i + 1]) {
-            return colors[i];
-        }
-    }
-    return colors[colors.length - 1];  
-}
-
-fetch('../Data/migration_data.geojson')
-    .then(response => response.json())
-    .then(data => {
-        L.geoJson(data, {
-            style: function(feature) {
-                var value = feature.properties.net_migration; 
-                return {
-                    fillColor: getColorForValue(value), 
-                    weight: .01,
-              
-                    fillOpacity: .7
-                };
+    // Function to get color based on value with explicit binning
+    function getColorForValue(value) {
+        for (let i = 0; i < bins.length - 1; i++) {
+            if (value >= bins[i] && value < bins[i + 1]) {
+                return colors[i];
             }
-        }).addTo(map);
-    });
+        }
+        return colors[colors.length - 1];  
+    }
 
+    fetch('../Data/migration_data.geojson')
+        .then(response => response.json())
+        .then(data => {
+            L.geoJson(data, {
+                style: function(feature) {
+                    var value = feature.properties.net_migration; 
+                    return {
+                        fillColor: getColorForValue(value), 
+                        weight: .01,
+                
+                        fillOpacity: .7
+                    };
+                }
+            }).addTo(map);
+        });
 });
