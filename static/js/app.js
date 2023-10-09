76cf48d992306023fc13ed6969af5ee79a1ce74a
Nicole-Ham <NLHammack@gmail.com>
2023-10-09 12:39:46 -0700

    moved html files to templates
    added routes for all html views in app.py
    
    fixed js to not call scatter plot code when selOutcome is updated

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,14 @@
-
-const vax_data = d3.json("http://127.0.0.1:5000/api/v1.0/vaccine_data")
-vax_data.then(function(data) {
-  
+const vax_data = d3.json("http://127.0.0.1:5000/api/v1.0/vaccine_data");
+vax_data.then(function (data) {
   //console.log(data);
   //console.log(Object.values(data.county));
   //console.log(Object.values(data.cumulative_up_to_date_count));
 });
 
-let vax_data_arr = null
+let vax_data_arr = null;
 
 //   let trace = [{
-    
+
 //     x: data.month,
 //     y: data.cumulative_up_to_date_count,
 //     type: "scatter"
@@ -25,9 +23,8 @@ let vax_data_arr = null
 //--------------------------------
 
 function uniqueArray4(a) {
-  
   return [...new Set(a)];
-};
+}
 //https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
 
 //--------------------------------
@@ -35,119 +32,117 @@ function uniqueArray4(a) {
 //--------------------------------
 
 function barTrace(x_vals, y_vals, year_val) {
-
   let trace = {
-    
     x: x_vals,
     y: y_vals,
     name: year_val,
-    type: "bar"
+    type: "bar",
   };
 
   return trace;
-};
+}
 
 function plotBar(outcome, years, type_arr, month_arr, year_arr, unique_months_arr) {
-  
   let type_outcome_2020 = {};
   let type_outcome_2021 = {};
   let type_outcome_2022 = {};
   let type_outcome_2023 = {};
 
   for (i in unique_months_arr) {
-
     type_outcome_2020[unique_months_arr[i]] = 0;
     type_outcome_2021[unique_months_arr[i]] = 0;
     type_outcome_2022[unique_months_arr[i]] = 0;
     type_outcome_2023[unique_months_arr[i]] = 0;
-  };
+  }
 
   for (let i = 0; i < type_arr.length; i++) {
-    
     if (type_arr[i] == outcome) {
-  
-      if (year_arr[i] == 2020) {type_outcome_2020[month_arr[i]] += 1;}
-      else if (year_arr[i] == 2021) {type_outcome_2021[month_arr[i]] += 1;}
-      else if (year_arr[i] == 2022) {type_outcome_2022[month_arr[i]] += 1;}
-      else {type_outcome_2023[month_arr[i]] += 1;}
+      if (year_arr[i] == 2020) {
+        type_outcome_2020[month_arr[i]] += 1;
+      } else if (year_arr[i] == 2021) {
+        type_outcome_2021[month_arr[i]] += 1;
+      } else if (year_arr[i] == 2022) {
+        type_outcome_2022[month_arr[i]] += 1;
+      } else {
+        type_outcome_2023[month_arr[i]] += 1;
+      }
     }
-  };
-  let type_outcome_dict = {2020: type_outcome_2020, 2021: type_outcome_2021, 2022: type_outcome_2022, 2023: type_outcome_2023};
+  }
+  let type_outcome_dict = { 2020: type_outcome_2020, 2021: type_outcome_2021, 2022: type_outcome_2022, 2023: type_outcome_2023 };
 
-  let type_bar_data = [] ;
+  let type_bar_data = [];
   for (i in years) {
-
     type_bar_data.push(barTrace(Object.keys(type_outcome_dict[years[i]]), Object.values(type_outcome_dict[years[i]]), years[i]));
-  };
+  }
 
   let bar_layout = {
-
-    title: {text:'Hospitalizations Over Time'},
-    xaxis: {title:{
-      text: 'Month',
-      standoff: 20
-    }},
-    yaxis: {title:{
-      text: 'Total Hospitalizations',
-      standoff: 10
-    }},
+    title: { text: "Hospitalizations Over Time" },
+    xaxis: {
+      title: {
+        text: "Month",
+        standoff: 20,
+      },
+    },
+    yaxis: {
+      title: {
+        text: "Total Hospitalizations",
+        standoff: 10,
+      },
+    },
 
     autosize: false,
     width: 525,
     height: 500,
     margin: {
-    l: 75,
-    r: 50,
-    b: 120,
-    t: 100,
-    pad: 4
-    }
+      l: 75,
+      r: 50,
+      b: 120,
+      t: 100,
+      pad: 4,
+    },
   };
 
   Plotly.newPlot("bar", type_bar_data, bar_layout);
-};
+}
 
 //--------------------------------
 //--------- Case Surv ------------
 //--------------------------------
 
-const case_data = d3.json("http://127.0.0.1:5000/api/v1.0/case_surv")
-case_data.then(function(data) {
-
+const case_data = d3.json("http://127.0.0.1:5000/api/v1.0/case_surv");
+case_data.then(function (data) {
   let data_length = Object.values(data.age_group).length;
 
   //--------------------------------
   //--------- Dropdown -------------
   //--------------------------------
 
-  let all_years = [2020, 2021, 2022, 2023]; 
+  let all_years = [2020, 2021, 2022, 2023];
 
   // let selector_start_year = d3.select("#selStart");
   // for(let i =0; i < counties.length; i++) {
-        
+
   //   selector_county.append("option").text(counties[i]).property("value", counties[i]);
   // };
 
   let county_arr = Object.values(data.county);
   // county_arr.push(" ALL COUNTIES");
-  let counties = uniqueArray4(county_arr); // get the unique counties, 
-  counties = counties.sort();//sort them
+  let counties = uniqueArray4(county_arr); // get the unique counties,
+  counties = counties.sort(); //sort them
 
   let selector_county = d3.select("#selCounty");
-  selector_county.append('option').text('ALL COUNTIES')
-  // add an option element for each county 
-  for(let i =0; i < counties.length; i++) {
-        
+  selector_county.append("option").text("ALL COUNTIES");
+  // add an option element for each county
+  for (let i = 0; i < counties.length; i++) {
     selector_county.append("option").text(counties[i]).property("value", counties[i]);
-  };
+  }
 
   let outcomes = ["Yes", "No", "Unknown"];
 
   let selector_outcome = d3.select("#selOutcome");
-  for(let i =0; i < outcomes.length; i++) {
-        
+  for (let i = 0; i < outcomes.length; i++) {
     selector_outcome.append("option").text(outcomes[i]).property("value", outcomes[i]);
-  };
+  }
 
   //--------------------------------
   //--------- Starter Graph Info ---
@@ -184,38 +179,36 @@ case_data.then(function(data) {
   d3.select("#selHosp").on("change", updateHosp);
   d3.select("#selCounty").on("change", updateGen);
   d3.select("#selOutcome").on("change", updateGen);
-  
- 
+
   // d3.select("#selOutcome").on("change", updateBar);
   // //d3.select("#selType").on("change", updateBar);
   // //d3.select()
   // d3.select("#currSettings").on("change", updateBar);
-  
+
   // // let curr_county_text = d3.select("#currSettings");
   // // curr_county_text.html(`&nbsp;&nbsp;&nbsp; <strong>Current County:</strong> ${selected_county}`);
-  
+
   function updateDeath() {
-    
     updateGen("death_yn");
-  };
-  
+  }
+
   function updateHosp() {
-    
     updateGen("hosp_yn");
-  };
-  
+  }
+
   //this function is called when the selCounty value is changed
-  function updateGen($event) {
-
-    const value = $event?.target?.value
-    updateBar($event)
+  function updateGen(/** @type {Event} */ $event) {
+    const value = $event?.target?.value;
+    updateBar($event);
     updateCurr($event);
-    makeScatterPlot(vax_data_arr,value)
 
-  };
+    // only call makeScatter if the change is from county
+    if (/** @type {HTMLElement} */ ($event.target).getAttribute("id") === "selCounty") {
+      makeScatterPlot(vax_data_arr, value);
+    }
+  }
 
   function updateCurr(type) {
-
     let county_dropdown = d3.select("#selCounty");
     let selected_county = county_dropdown.property("value");
 
@@ -225,35 +218,32 @@ case_data.then(function(data) {
     let select_curr_settings = d3.select("#currSettings");
     let new_curr_settings_text = `<strong>${data_keys[0]}</strong> ${selected_county} <br> <strong>${data_keys[1]}</strong> ${selected_outcome}`;
     select_curr_settings.html(new_curr_settings_text);
-  };
+  }
 
   //------------------------
   //----- Update Plots -----
   //------------------------
 
   function updateBar(type) {
-
     let county_dropdown = d3.select("#selCounty");
     let selected_county = county_dropdown.property("value");
 
     let outcome_dropdown = d3.select("#selOutcome");
     let selected_outcome = outcome_dropdown.property("value");
 
-    if (selected_county == "ALL COUNTIES") {plotBar(selected_outcome, all_years, all_hosp_arr, all_months_arr, all_years_arr, unique_months);}
-    else {
-
+    if (selected_county == "ALL COUNTIES") {
+      plotBar(selected_outcome, all_years, all_hosp_arr, all_months_arr, all_years_arr, unique_months);
+    } else {
       let curr_hosp_arr = [];
       let curr_months_arr = [];
       let curr_years_arr = [];
       for (let i = 0; i < data_length; i++) {
-
         if (data.county[i] == selected_county) {
-
           curr_hosp_arr.push(data.hosp_yn[i]);
           curr_months_arr.push(data.month[i]);
           curr_years_arr.push(data.year[i]);
-        };
-      };
+        }
+      }
 
       plotBar(selected_outcome, all_years, curr_hosp_arr, curr_months_arr, curr_years_arr, unique_months);
     }
@@ -269,76 +259,63 @@ case_data.then(function(data) {
 // .then(data => {
 // Call a function to create the line chart with the fetched data
 /**
- * line chart is 
+ * line chart is
  * x:counties-month
  * y: vaccine_couts
  */
 
-/** 
+/**
  * @typedef {Object} Vaccine
-*  @property {any} year
-*  @property {any} month
-*  @property {any} county
-*  @property {any} cumulative_total_doses
-*  @property {any} cumulative_fully_vaccinated
-*  @property {any} cumulative_at_least_one_dose
-*  @property {any} cumulative_up_to_date_count
+ *  @property {any} year
+ *  @property {any} month
+ *  @property {any} county
+ *  @property {any} cumulative_total_doses
+ *  @property {any} cumulative_fully_vaccinated
+ *  @property {any} cumulative_at_least_one_dose
+ *  @property {any} cumulative_up_to_date_count
  */
 
-const vaccination_data = d3.json("http://127.0.0.1:5000/api/v1.0/vaccine_data")
-vaccination_data.then((vaccines)=>{
-  vax_data_arr = vaccines
-  makeScatterPlot(vax_data_arr)
+const vaccination_data = d3.json("http://127.0.0.1:5000/api/v1.0/vaccine_data");
+vaccination_data.then((vaccines) => {
+  vax_data_arr = vaccines;
+  makeScatterPlot(vax_data_arr);
 });
 
-function makeScatterPlot (/** @type {Vaccine[]} */ vaccines_list,county=null) {
-  
-  console.log('vaccine_data ready for line chart',vaccines_list.length,county);
+function makeScatterPlot(/** @type {Vaccine[]} */ vaccines_list, county = null) {
+  console.log("vaccine_data ready for line chart", vaccines_list.length, county);
   // console.log(Object.values(data.Date));
   // console.log(Object.values(data.cumulative_up_to_date_count));
 
   //
   let trace = [
     {
-
-    x: vaccines_list.filter(data=> ![null,undefined,'','ALL COUNTIES'].includes(county)? data.county===county:true).map((x)=> `${x.year}-${x.month}`),
-    y: vaccines_list.filter(data=> ![null,undefined,'','ALL COUNTIES'].includes(county)? data.county===county:true).map(x=>x.cumulative_up_to_date_count),
-    name:'up_to_date',
-    type: "scatter",
-    marker: { size: 10 }
-  },
+      x: vaccines_list.filter((data) => (![null, undefined, "", "ALL COUNTIES"].includes(county) ? data.county === county : true)).map((x) => `${x.year}-${x.month}`),
+      y: vaccines_list.filter((data) => (![null, undefined, "", "ALL COUNTIES"].includes(county) ? data.county === county : true)).map((x) => x.cumulative_up_to_date_count),
+      name: "up_to_date",
+      type: "scatter",
+      marker: { size: 10 },
+    },
     {
-
-    x: vaccines_list.filter(data=> ![null,undefined,'','ALL COUNTIES'].includes(county)? data.county===county:true).map((x)=> `${x.year}-${x.month}`),
-    y: vaccines_list.filter(data=> ![null,undefined,'','ALL COUNTIES'].includes(county)? data.county===county:true).map(x=>x.cumulative_fully_vaccinated),
-    type: "scatter",
-    name: 'full_vaxed',
-    marker: { size: 10 }
-  },
-  {
-
-    x: vaccines_list.filter(data=> ![null,undefined,'','ALL COUNTIES'].includes(county)? data.county===county:true).map((x)=> `${x.year}-${x.month}`),
-    y: vaccines_list.filter(data=> ![null,undefined,'','ALL COUNTIES'].includes(county)? data.county===county:true).map(x=>x.cumulative_total_doses),
-    type: "scatter",
-    name: 'total doses',
-    marker: { size: 10 }
-  }
-];
+      x: vaccines_list.filter((data) => (![null, undefined, "", "ALL COUNTIES"].includes(county) ? data.county === county : true)).map((x) => `${x.year}-${x.month}`),
+      y: vaccines_list.filter((data) => (![null, undefined, "", "ALL COUNTIES"].includes(county) ? data.county === county : true)).map((x) => x.cumulative_fully_vaccinated),
+      type: "scatter",
+      name: "full_vaxed",
+      marker: { size: 10 },
+    },
+    {
+      x: vaccines_list.filter((data) => (![null, undefined, "", "ALL COUNTIES"].includes(county) ? data.county === county : true)).map((x) => `${x.year}-${x.month}`),
+      y: vaccines_list.filter((data) => (![null, undefined, "", "ALL COUNTIES"].includes(county) ? data.county === county : true)).map((x) => x.cumulative_total_doses),
+      type: "scatter",
+      name: "total doses",
+      marker: { size: 10 },
+    },
+  ];
 
   const layout = {
-    title: 'County Vaccination Numbers',
-    xaxis: { title: 'Date' },
-    yaxis: { title: 'Vaccination Counts' }
-};
+    title: "County Vaccination Numbers",
+    xaxis: { title: "Date" },
+    yaxis: { title: "Vaccination Counts" },
+  };
 
-  Plotly.newPlot("scatter", trace,layout);
-
-
+  Plotly.newPlot("scatter", trace, layout);
 }
-
-
-
-
-
-
-
