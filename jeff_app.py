14dbb87884faba0cd2f42f4a10e839f5c7d6bb11
Jeff North <jnorthdata@gmail.com>
2023-10-10 16:03:18 -0700

    map working

--- jeff_app.py
+++ jeff_app.py
@@ -1,93 +1,91 @@
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine
 from flask import Flask, jsonify
 from flask_cors import cross_origin, CORS
-from sqlalchemy import MetaData, inspect
-import sqlite3
 import geopandas as gpd
 import pandas as pd
+import json
 
-
-
-#################################################
 # Database Setup
-
 engine = create_engine("sqlite:///Data/CA_COVID_data.sqlite")
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
+# Reflect the tables
+Base.prepare(engine, reflect=True)
 
 app = Flask(__name__)
 CORS(app)
 
-#################################################
+# Preload the data
+MigrationData = Base.classes.county_migrations
+session = Session(engine)
+all_data = pd.read_sql(session.query(MigrationData).statement, session.bind)
+session.close()
 
-# inspector = inspect(engine)
-# print(inspector.get_table_names())
+# Preload county geometries
+counties_gdf = gpd.read_file('Data/us_counties_geometry.geojson')
 
 
+@app.route('/api/v1.0/migration_data/<int:year>')
+def migration_data(year):
+    # Filter the preloaded data by the provided year
+    filtered_data = all_data[all_data['year'] == year]
 
-# RETURN DATA TO JAVASCRIPT APP
-
-@app.route('/api/v1.0/migration_data')
-def migration_data():
-    # Connect to the SQLite database and read the 'county_migrations' table
-    conn = sqlite3.connect('Data/CA_COVID_data.sqlite')
-    query = "SELECT * FROM county_migrations"
-    migrations_df = pd.read_sql(query, conn)
-    conn.close()
-
-    # Load the GeoJSON with county geometries
-    counties_gdf = gpd.read_file('Data/us_counties_geometry.geojson')
-
-    # Merge the two datasets on the appropriate column (e.g., 'geo_id' or 'fips')
-    merged_gdf = counties_gdf.merge(migrations_df, left_on="GEOID", right_on="fips")
+    # Merge the county geometries with the filtered data
+    merged_gdf = counties_gdf.merge(filtered_data, left_on="GEOID", right_on="fips")
 
     # Convert the merged GeoDataFrame to GeoJSON format
     merged_geojson = merged_gdf.to_json()
 
-    return jsonify(merged_geojson)
+    # Convert the string representation of GeoJSON to a JSON object
+    json_data = json.loads(merged_geojson)
 
-
-
-
-
-
-
-@app.route('/api/v1.0/table_data', methods=['GET'])
-@cross_origin()
-def table_data():
-    # Access the reflected table class
-    MigrationData = Base.classes.county_migrations
-
-    # Start a session and query all the data
-    session = Session(engine)
-    results = session.query(MigrationData).all()
-    session.close()
-
-    # Convert the queried results to a list of dictionaries
-    data_list = []
-    for row in results:
-        # Convert each row to a dictionary (while excluding '_sa_instance_state')
-        data_dict = {column: getattr(row, column) for column in row.__dict__.keys() if column != '_sa_instance_state'}
-        data_list.append(data_dict)
-
-    return jsonify(data_list)
-
-
-
-@app.route("/api/v1.0/tables")
-@cross_origin(origin='*')
-def tables():
-    # Get a list of all table names using the 'inspect' function
-    inspector = inspect(engine)
-    tables = inspector.get_table_names()
-    return jsonify(tables)
-
+    return jsonify(json_data)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.route('/api/v1.0/table_data', methods=['GET'])
+# @cross_origin()
+# def table_data():
+#     # Access the reflected table class
+#     MigrationData = Base.classes.county_migrations
+
+#     # Start a session and query all the data
+#     session = Session(engine)
+#     results = session.query(MigrationData).all()
+#     session.close()
+
+#     # Convert the queried results to a list of dictionaries
+#     data_list = []
+#     for row in results:
+#         # Convert each row to a dictionary (while excluding '_sa_instance_state')
+#         data_dict = {column: getattr(row, column) for column in row.__dict__.keys() if column != '_sa_instance_state'}
+#         data_list.append(data_dict)
+
+#     return jsonify(data_list)
+
+
+
+# @app.route("/api/v1.0/tables")
+# @cross_origin(origin='*')
+# def tables():
+#     # Get a list of all table names using the 'inspect' function
+#     inspector = inspect(engine)
+#     tables = inspector.get_table_names()
+#     return jsonify(tables)
